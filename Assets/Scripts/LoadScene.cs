5028fec7f511c27cc01f941498ae932e71aa9287
Khalid Hassan <khalidhsoliman@gmail.com>
2019-02-10 09:36:28 +0200

    Add SplashScreen Scene

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class LoadScene : MonoBehaviour {
+
+	
+	void Start () {
+        Invoke("Load", 3.0f);	
+	}
+
+    private void Load()
+    {
+        SceneManager.LoadScene(1);
+    }
+	
+
+}
